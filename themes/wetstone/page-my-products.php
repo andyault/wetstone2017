652d19a250b7cc28207643f3e59bdfb1286defb2
Andrew Ault <andrew.ault@outlook.com>
2017-09-23 20:03:12 -0400

    working on portal

--- themes/wetstone/page-my-products.php
+++ themes/wetstone/page-my-products.php
@@ -1,3 +1,17 @@
-<?php get_header(); ?>
+<?php
+	get_header();
+
+	$products = 1;
+?>
+
+<section class="site-content site-content-padded">
+	<ul class="myproducts-list">
+		<?php
+			var_dump(wp_get_current_user());
+			echo "\n--\n";
+			var_dump(get_user_mata($wp_get_current_user->ID));
+		?>
+	</ul>
+</section>
 
 <?php get_footer(); ?>
