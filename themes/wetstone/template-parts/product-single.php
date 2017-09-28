82a0f78ddff1366e77971295026457d1bbebc2a3
Andrew Ault <andrew.ault@outlook.com>
2017-09-28 14:34:37 -0400

    left debug code in on accident

--- themes/wetstone/template-parts/product-single.php
+++ themes/wetstone/template-parts/product-single.php
@@ -1,11 +1,6 @@
 <?php $productColor = get_post_meta($post->ID, 'product_color', true); ?>
 
-<?php var_dump(get_post_meta($post->ID)); ?>
-
 <!-- todo: side buttons (overview, screenshots, free trial, etc) -->
-
-<!-- <section class="product-single-banner" style="background: <?php echo $productColor; ?>;">
-</section> -->
 
 <section class="product-single-description">
 	<h1 class="section-header <?php if($post->ID != 141) echo 'wetstone-font'; ?>"><?php the_title(); ?></h1>
