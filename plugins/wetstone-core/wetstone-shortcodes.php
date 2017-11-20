f86d956561a04fb5165ac72a0de8fa92966df4be
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2017-11-20 13:41:08 -0500

    Adding Dataset shortcode for GDS

--- plugins/wetstone-core/wetstone-shortcodes.php
+++ plugins/wetstone-core/wetstone-shortcodes.php
@@ -185,4 +185,73 @@ function wetstone_add_page_excerpts() {
 	add_post_type_support('page', 'excerpt');
 }
 
+
+
 add_action('init', 'wetstone_add_page_excerpts');
+
+
+//add Dataset info
+add_shortcode("GDS", "display_GDS");
+ 
+function display_GDS($atr){
+	
+        $dataset = '../protected/Gargoyle Investigator/Dataset Updates';
+		$pdfEng = '../protected/Gargoyle Investigator/Release Notes';
+		$pdfSpa = '../protected/Gargoyle Investigator/Release Notes - Spanish';
+		$hashes = '../protected/Gargoyle Investigator/Supplemental Gargoyle Hashes';
+		
+		$datasetFiles = scan_dir($dataset);
+		$pdfEngFiles = scan_dir($pdfEng);
+		$pdfSpaFiles = scan_dir($pdfSpa);
+		$hashFiles = scan_dir($hashes);		
+		
+		$aca_table = "";
+		$aca_table .= '<table style="border: 1px solid black">';
+		$aca_table .= '<tr style="border: 1px solid black">';
+		$aca_table .= '<th colspan="2" style="padding-left:7px;">Supplemental Gargoyle Hashes</th>';
+		$aca_table .= '</tr>';
+		$aca_table .= '<tr style="border: 1px solid black">';
+		$aca_table .= '<td style="padding-left:7px; width:475px; height:20px;"><a href="'.$hashes.'/'.$hashFiles[0].'" target="_blank" style="text-decoration:none; color:green;">Hash File</a></td><td><img src="http://localhost/wordpress/wp-content/uploads/2017/11/zip-icon.png" /></td>';
+		$aca_table .= '</tr>';
+		$aca_table .= '</table>';
+		
+		echo $aca_table;
+		
+		for ($aca_i = 0; $aca_i < 3; $aca_i++) {
+			$aca_header = explode("_",$datasetFiles[$aca_i]);
+			
+			$aca_table = "";
+			$aca_table .= '<table style="border: 1px solid black">';
+			$aca_table .= '<tr style="border: 1px solid black">';
+			$aca_table .= '<th colspan="2" style="padding-left:7px;">'. $aca_header[1] ." ". $aca_header[0] .'</th>';
+			$aca_table .= '</tr>';
+			$aca_table .= '<tr style="border: 1px solid black">';
+			$aca_table .= '<td style="padding-left:7px; width:475px; height:20px;"><a href="'.$dataset.'/'.$datasetFiles[$aca_i].'" target="_blank" style="text-decoration:none; color:green;">Dataset File</a></td><td><img src="http://localhost/wordpress/wp-content/uploads/2017/11/cab-icon.png" /></td>';
+			$aca_table .= '</tr>';
+			$aca_table .= '<tr style="border: 1px solid black">';
+			$aca_table .= '<td style="padding-left:7px; width:475px; height:20px;"><a href="'.$pdfEng.'/'.$pdfEngFiles[$aca_i].'" target="_blank" style="text-decoration:none; color:green;">Dataset Release Notes - English</a></td><td><img src="http://localhost/wordpress/wp-content/uploads/2017/11/pdf-icon.png" /></td>';
+			$aca_table .= '</tr>';
+			$aca_table .= '<tr style="border: 1px solid black">';
+			$aca_table .= '<td style="padding-left:7px; width:475px; height:20px;"><a href="'.$pdfSpa.'/'.$pdfSpaFiles[$aca_i].'" target="_blank" style="text-decoration:none; color:green;">Dataset Release Notes - Spanish</a></td><td><img src="http://localhost/wordpress/wp-content/uploads/2017/11/pdf-icon.png" /></td>';
+			$aca_table .= '</tr>';
+			$aca_table .= '</table>';
+			
+			echo $aca_table;
+		}
+
+}
+
+function scan_dir($dir) {
+    $ignored = array('.', '..');
+
+    $files = array();    
+    foreach (scandir($dir) as $file) {
+        if (in_array($file, $ignored)) continue;
+        $files[$file] = filemtime($dir . '/' . $file);
+    }
+
+    arsort($files);
+    $files = array_keys($files);
+
+    return ($files) ? $files : false;
+}
