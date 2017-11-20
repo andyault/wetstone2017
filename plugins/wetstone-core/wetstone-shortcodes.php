c00847f349852f340f0908644f63d03dc63f7468
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2017-11-20 14:14:06 -0500

    trying to firgure out why files are not reading

--- plugins/wetstone-core/wetstone-shortcodes.php
+++ plugins/wetstone-core/wetstone-shortcodes.php
@@ -196,47 +196,12 @@ add_shortcode("GDS", "display_GDS");
 function display_GDS($atr){
 	
         $dataset = '../../protected';
-		$pdfEng = '../../protected';
-		$pdfSpa = '../../protected';
-		$hashes = '../../protected';
+		$pdfEng = '../../protected/Gargoyle Investigator/Release Notes';
+		$pdfSpa = '../../protected/Gargoyle Investigator/Release Notes - Spanish';
+		$hashes = '../../protected/Gargoyle Investigator/Supplemental Gargoyle Hashes';
 		
 		$datasetFiles = scandir($dataset);
-		$pdfEngFiles = scandir($pdfEng);
-		$pdfSpaFiles = scandir($pdfSpa);
-		$hashFiles = scandir($hashes);		
-		
-		$aca_table = "";
-		$aca_table .= '<table style="border: 1px solid black">';
-		$aca_table .= '<tr style="border: 1px solid black">';
-		$aca_table .= '<th colspan="2" style="padding-left:7px;">Supplemental Gargoyle Hashes</th>';
-		$aca_table .= '</tr>';
-		$aca_table .= '<tr style="border: 1px solid black">';
-		$aca_table .= '<td style="padding-left:7px; width:475px; height:20px;"><a href="'.$hashes.'/'.$hashFiles[0].'" target="_blank" style="text-decoration:none; color:green;">Hash File</a></td><td><img src="http://104.131.53.0/wp-content/uploads/2017/11/zip-icon.png" /></td>';
-		$aca_table .= '</tr>';
-		$aca_table .= '</table>';
-		
-		echo $aca_table;
-		
-		for ($aca_i = 2; $aca_i < 5; $aca_i++) {
-			$aca_header = explode("_",$datasetFiles[$aca_i]);
-			
-			$aca_table = "";
-			$aca_table .= '<table style="border: 1px solid black">';
-			$aca_table .= '<tr style="border: 1px solid black">';
-			$aca_table .= '<th colspan="2" style="padding-left:7px;">'. $aca_header[1] ." ". $aca_header[0] .'</th>';
-			$aca_table .= '</tr>';
-			$aca_table .= '<tr style="border: 1px solid black">';
-			$aca_table .= '<td style="padding-left:7px; width:475px; height:20px;"><a href="'.$dataset.'/'.$datasetFiles[$aca_i].'" target="_blank" style="text-decoration:none; color:green;">Dataset File</a></td><td><img src="http://104.131.53.0/wp-content/uploads/2017/11/cab-icon.png" /></td>';
-			$aca_table .= '</tr>';
-			$aca_table .= '<tr style="border: 1px solid black">';
-			$aca_table .= '<td style="padding-left:7px; width:475px; height:20px;"><a href="'.$pdfEng.'/'.$pdfEngFiles[$aca_i].'" target="_blank" style="text-decoration:none; color:green;">Dataset Release Notes - English</a></td><td><img src="http://104.131.53.0/wp-content/uploads/2017/11/pdf-icon.png" /></td>';
-			$aca_table .= '</tr>';
-			$aca_table .= '<tr style="border: 1px solid black">';
-			$aca_table .= '<td style="padding-left:7px; width:475px; height:20px;"><a href="'.$pdfSpa.'/'.$pdfSpaFiles[$aca_i].'" target="_blank" style="text-decoration:none; color:green;">Dataset Release Notes - Spanish</a></td><td><img src="http://104.131.53.0/wp-content/uploads/2017/11/pdf-icon.png" /></td>';
-			$aca_table .= '</tr>';
-			$aca_table .= '</table>';
-			
-			echo $aca_table;
-		}
+
+		printf $datasetFiles;
 
 }
