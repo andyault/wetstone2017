9addbe684bd13cdc47da2e131890afbed3db0a11
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2018-01-17 10:39:36 -0500

    footer

--- themes/wetstone/footer.php
+++ themes/wetstone/footer.php
@@ -30,7 +30,7 @@
 
 						foreach($pages as $page) {
 							echo '<li>';
-							echo $pages;
+							echo $pages[0];
 							if(gettype($page) == 'string')
 								$page = get_page_by_path($page);
 
