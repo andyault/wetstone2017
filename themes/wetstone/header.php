576fd658a548aac02efa4d099052ed60ae9c241e
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2018-02-05 11:44:57 -0500

    header

--- themes/wetstone/header.php
+++ themes/wetstone/header.php
@@ -78,7 +78,7 @@
 						$isActive = is_page($id) || $page->post_parent == $id;
 						$activeClass = $isActive ? 'active' : '';
 
-						if ($id == 613) {
+						if ($id == 613 || $id == 612) {
 						} else {
 							return sprintf(
 								'<a href="%s" class="header-link link link-header-site %s">%s</a>', 
