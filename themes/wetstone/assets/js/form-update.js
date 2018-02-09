bb3be08c1f210ea38b9a4d09113cc951e2bc0fc9
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2018-02-09 09:42:04 -0500

    form

--- themes/wetstone/assets/js/form-update.js
+++ themes/wetstone/assets/js/form-update.js
@@ -1,3 +1,9 @@
 function country_update() {
-		
+	var country = document.getElementById("countryID").value;
+	
+	if (country == "United States") {
+		alert("USA");
+	} else {
+		alert("Not USA");
+	}
 }
