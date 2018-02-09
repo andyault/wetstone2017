54feb8ea0a875927b8848acaa91ab3646b33f897
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2018-02-09 10:46:46 -0500

    form

--- themes/wetstone/assets/js/form-update.js
+++ themes/wetstone/assets/js/form-update.js
@@ -16,7 +16,7 @@ function country_update() {
 
 function validatePhone() {
 	var phoneNum = document.getElementById("phoneVal").value;
-	var phoneno = "/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/";
+	var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 		  if(phoneNum.value.match(phoneno)){
 				alert("Good Number");
 				return true;
