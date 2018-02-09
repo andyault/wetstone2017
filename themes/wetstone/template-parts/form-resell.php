f68862a3b43a120bfd743da27c1ba5aedeffb3bc
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2018-02-09 09:48:59 -0500

    form

--- themes/wetstone/template-parts/form-resell.php
+++ themes/wetstone/template-parts/form-resell.php
@@ -313,7 +313,64 @@
 			<td>
 				<div class="form-multiple">
 					<?php
-						echo wetstone_form_make_input('state', 'text', 'State/Province', 'MD', true, ['size' => 7]);
+						echo wetstone_form_make_select_state(
+							'state', 
+							'State/Province', 
+							[
+								'AL',
+								'AK',
+								'AZ',
+								'AR',
+								'CA',
+								'CO',
+								'CT',
+								'DE',
+								'DC',
+								'FL',
+								'GA',
+								'HI',
+								'ID',
+								'IL',
+								'IN',
+								'IA',
+								'KS',
+								'KY',
+								'LA',
+								'ME',
+								'MD',
+								'MA',
+								'MI',
+								'MN',
+								'MS',
+								'MO',
+								'MT',
+								'NE',
+								'NV',
+								'NH',
+								'NJ',
+								'NM',
+								'NY',
+								'NC',
+								'ND',
+								'OH',
+								'OK',
+								'OR',
+								'PA',
+								'RI',
+								'SC',
+								'SD',
+								'TN',
+								'TX',
+								'UT',
+								'VT',
+								'VA',
+								'WA',
+								'WV',
+								'WI',
+								'WY'
+							],
+						true
+					);
 						echo wetstone_form_make_input('zip', 'text', 'Zip/Postal', '20500', true, ['size' => 10]);
 					?>
 				</div>
