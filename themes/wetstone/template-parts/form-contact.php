8acac6445fc8516df1413a149015546aae95a170
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2018-03-30 15:31:35 -0400

    center capcha

--- themes/wetstone/template-parts/form-contact.php
+++ themes/wetstone/template-parts/form-contact.php
@@ -387,7 +387,7 @@
 
 		<tr>
 			<td colspan="2" class="table-footer">
-				<div class="inline-flex">	<?php echo apply_filters( 'gglcptch_display_recaptcha', '' ); ?></div>
+				<center><?php echo apply_filters( 'gglcptch_display_recaptcha', '' ); ?></center>
 				<div class="inline-flex">				
 					<button type="reset" class="form-reset link link-button link-button-input link-button-grey">Reset</button>
 					<button type="submit" class="link link-button link-button-input">Submit</button>
