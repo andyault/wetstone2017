1c6cc38917138cf4414ffc3bf0ad61308e925215
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2018-03-30 15:46:11 -0400

    capcha

--- plugins/wetstone-core/wetstone-form-handling.php
+++ plugins/wetstone-core/wetstone-form-handling.php
@@ -61,34 +61,42 @@ add_action('admin_post_nopriv_wetstone-contact-form', 'wetstone_post_contact_for
 
 //  resell form
 function wetstone_post_resell_form() {
-	if(!wp_verify_nonce($_POST['_wpnonce'], 'wetstone-resell-form'))
-		return wp_nonce_ays('wetstone-resell-form');
+	
+		$check_result = apply_filters( 'gglcptch_verify_recaptcha', true, 'string' );
+		 if ( true === $check_result ) { /* the reCAPTCHA answer is right */
+			echo '';
+		
+		if(!wp_verify_nonce($_POST['_wpnonce'], 'wetstone-resell-form'))
+			return wp_nonce_ays('wetstone-resell-form');
 
-	//sanitize inputs
-	$data = wetstone_sanitize_post([
-		'subject', 'fname', 'lname', 'company', 'website', 'phone', 'email', 'address1', 'address2', 'city', 'state', 
-		'zip', 'country', 'referrer', 'customers', 'marketing', 'description', 'territories'
-	]);
+		//sanitize inputs
+		$data = wetstone_sanitize_post([
+			'subject', 'fname', 'lname', 'company', 'website', 'phone', 'email', 'address1', 'address2', 'city', 'state', 
+			'zip', 'country', 'referrer', 'customers', 'marketing', 'description', 'territories'
+		]);
 
-	//turn into pretty table
-	$emailWidth = wetstone_get_option('form_handling', 'email_width');
+		//turn into pretty table
+		$emailWidth = wetstone_get_option('form_handling', 'email_width');
 
-	$subject = wetstone_pop_value($data, 'subject');
+		$subject = wetstone_pop_value($data, 'subject');
 
-	$fields = '<pre>';
-	$fields .= wetstone_columnify($data);
+		$fields = '<pre>';
+		$fields .= wetstone_columnify($data);
 
-	$fullName = $data['fname'] . ' ' . $data['lname'];
+		$fullName = $data['fname'] . ' ' . $data['lname'];
 
-	if(wetstone_send_mail($subject, $fullName, $data['email'], wordwrap($fields, $emailWidth)))
-		wp_redirect(get_permalink(get_page_by_path('thank-you')));
-	else {
-		//add error
-		$data['errmsg'] = 'Unable to send email - possible server error. Please wait and try again.';
+		if(wetstone_send_mail($subject, $fullName, $data['email'], wordwrap($fields, $emailWidth)))
+			wp_redirect(get_permalink(get_page_by_path('thank-you')));
+		else {
+			//add error
+			$data['errmsg'] = 'Unable to send email - possible server error. Please wait and try again.';
 
-		//go back to form with old data
-		wp_safe_redirect(wp_get_referer() . '?' . http_build_query($data));
-	}
+			//go back to form with old data
+			wp_safe_redirect(wp_get_referer() . '?' . http_build_query($data));
+		}
+		} else { /* the reCAPTCHA answer is wrong or there are some other errors */
+		echo $check_result; /* display the error message or do other necessary actions in case when the reCAPTCHA test was failed */
+		}
 }
 
 add_action('admin_post_wetstone-resell-form', 'wetstone_post_resell_form');
