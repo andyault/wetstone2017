89ab9dac7a11d0abe7c580ad32bbc06d522d00d2
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2018-08-14 09:45:26 -0400

    update

--- themes/wetstone/template-parts/form-contact.php
+++ themes/wetstone/template-parts/form-contact.php
@@ -387,7 +387,7 @@
 
 		<tr>
 			<td colspan="2" class="table-footer">
-				<center><?php echo apply_filters( 'gglcptch_display_recaptcha', '' ); ?></center>
+				<center>[bws_google_captcha]</center>
 				<div class="inline-flex">				
 					<button type="reset" class="form-reset link link-button link-button-input link-button-grey">Reset</button>
 					<button type="submit" class="link link-button link-button-input">Submit</button>
