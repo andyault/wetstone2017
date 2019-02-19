3e476699d785224787182198d6a8e2a73714bf77
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-02-19 15:10:43 -0500

    Update generate-key.php

--- themes/wetstone/generate-key.php
+++ themes/wetstone/generate-key.php
@@ -39,10 +39,10 @@
 $subject = 'WetStoneTech.com - ' . $productCode . ' License Key';
 
 		$headers = array('Content-Type: text/plain; charset=iso-8859-1; Content-Transfer-Encoding: 7bit');
-		$body = "\n". "Date/Time: " . $date . "\n";
-		$body .= "Customer Name: " . $fullname . "\n\r";
-		$body .= "Customer Email: " . $user->user_email . "\n\r";
-		$body .= "Product Code: " . $productCode . "\n\r";
+		$body = "\n". "Date/Time: " . $date . "\r";
+		$body .= "Customer Name: " . $fullname . "\r";
+		$body .= "Customer Email: " . $user->user_email . "\r";
+		$body .= "Product Code: " . $productCode . "\r";
 		$body .= "Registration Code: " . $_POST['regcode'] . PHP_EOL;		
 
 		//try to send
