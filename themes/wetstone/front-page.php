e73f2c594429eb1ad1f28e5fd277435a4b2e44a6
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-06-10 12:55:52 -0400

    video

--- themes/wetstone/front-page.php
+++ themes/wetstone/front-page.php
@@ -19,7 +19,7 @@
 				 <center>
 				 <br /><br />				 
 				 <br /><br />
-				 <video controls width="600" height="338" poster="<?php echo wetstone_get_asset('/img/background/poster.png'); ?>" autoplay loop>
+				 <video controls width="600" height="338" poster="<?php echo wetstone_get_asset('/img/background/poster.png'); ?>">
 					  <source src="<?php echo wetstone_get_asset('/img/background/Gargoyle_MP_Trial.mp4'); ?>" type="video/mp4" /><?php echo $err; ?>
 				</video> 
 
