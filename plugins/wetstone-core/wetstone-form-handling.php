8593431933b98f58e1652567cbb5ac1fbebe1d0a
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-08-13 06:42:35 -0400

    Update wetstone-form-handling.php

--- plugins/wetstone-core/wetstone-form-handling.php
+++ plugins/wetstone-core/wetstone-form-handling.php
@@ -142,45 +142,10 @@ add_action('admin_post_nopriv_wetstone-mp-demo', 'wetstone_mp_demo_form');
 //  resell form
 function wetstone_post_resell_form() {
 	
-		//$check_result = apply_filters( 'gglcptch_verify_recaptcha', true, 'string' );
-		// if ( true === $check_result ) { /* the reCAPTCHA answer is right */
-		//	echo '';
-		
-		if(!wp_verify_nonce($_POST['_wpnonce'], 'wetstone-resell-form'))
-			return wp_nonce_ays('wetstone-resell-form');
 
-		//sanitize inputs
-		$data = wetstone_sanitize_post([
-			'subject', 'fname', 'lname', 'company', 'website', 'phone', 'email', 'address1', 'address2', 'city', 'state', 
-			'zip', 'country', 'referrer', 'customers', 'marketing', 'comments', 'description', 'territories'
-		]);
-
-		//turn into pretty table
-		$emailWidth = wetstone_get_option('form_handling', 'email_width');
-
-		$subject = wetstone_pop_value($data, 'subject');
-
-		$fields = '<pre>';
-		$fields .= wetstone_columnify($data);
-
-		$fullName = $data['fname'] . ' ' . $data['lname'];
-
-		if(wetstone_send_mail($subject, $fullName, $data['email'], wordwrap($fields, $emailWidth)))
-			wp_redirect(get_permalink(get_page_by_path('thank-you')));
-		else {
-			//add error
-			$data['errmsg'] = 'Unable to send email - possible server error. Please wait and try again.';
-
-			//go back to form with old data
-			wp_safe_redirect(wp_get_referer() . '?' . http_build_query($data));
-		}
-	//	} else { /* the reCAPTCHA answer is wrong or there are some other errors */
-	//	echo $check_result; /* display the error message or do other necessary actions in case when the reCAPTCHA test was failed */
-	//	}
 }
 
-add_action('admin_post_wetstone-resell-form', 'wetstone_post_resell_form');
-add_action('admin_post_nopriv_wetstone-resell-form', 'wetstone_post_resell_form');
+
 
 
 //support page
