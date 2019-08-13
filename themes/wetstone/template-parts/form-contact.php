17bc2ce30fb9758a957bf9ddbf0b5e380ce0c741
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-08-13 11:48:24 -0400

    Update form-contact.php

--- themes/wetstone/template-parts/form-contact.php
+++ themes/wetstone/template-parts/form-contact.php
@@ -394,7 +394,7 @@
 
 		<tr>
 			<td colspan="2" class="table-footer">
-				<div class="mtcaptcha inline-flex"></div>	
+				<center><div class="mtcaptcha"></div></center>
 				<div class="inline-flex">									
 					<button type="reset" class="form-reset link link-button link-button-input link-button-grey">Reset</button>
 					<button type="submit" class="link link-button link-button-input">Submit</button>
