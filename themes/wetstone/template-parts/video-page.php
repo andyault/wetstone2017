71319b2798dd1f204a7aec42b30aa2a4fb03f2f0
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-11-12 13:56:29 -0500

    video page edits

---
+++ themes/wetstone/template-parts/video-page.php
@@ -0,0 +1,5 @@
+<div class="page-preview">
+	<h2><a href="<?php the_permalink(); ?>" class="link link-body"><?php the_title(); ?></a></h2>
+
+	<p class="body body-small"><?php echo get_the_excerpt(); ?></p>
+</div>
