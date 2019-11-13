178967de6b6be2bc452aaac45f5f354ac32557e4
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-11-13 09:22:25 -0500

    run

--- plugins/wetstone-core/wetstone-core.php
+++ plugins/wetstone-core/wetstone-core.php
@@ -133,32 +133,7 @@ function wetstone_add_options_page() {
 
 add_action('admin_menu', 'wetstone_add_options_page');
 
-add_filter( 'the_title', 'custom_posts_page_title', 10, 2 );
-/**
- * Sets custom title for the Posts page.
- *
- * @param  string $title Current title.
- * @param  int $id The post ID.
- *
- * @return string Modified title.
- */
-function custom_posts_page_title( $title, $id ) {
-    // get the id of Posts page.
-    $posts_page = get_option( 'video-library' );
 
-    // if we are not on an inner Posts page, abort.
-    if ( ! is_home() || is_front_page() ) {
-        return $title;
-    }
-
-    // if the current entry's ID matches with that of the Posts page..
-    if ( $id == $posts_page ) {
-        // set your new title here.
-        $title = 'WetStone Video Library';
-    }
-
-    return $title;
-}
 
 
 //include modules
