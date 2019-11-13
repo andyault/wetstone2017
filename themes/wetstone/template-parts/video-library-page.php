25dae4cd37e871c7d1f9d48fec0dc029c3acf974
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-11-13 08:53:54 -0500

    Update video-library-page.php

--- themes/wetstone/template-parts/video-library-page.php
+++ themes/wetstone/template-parts/video-library-page.php
@@ -1,3 +1,10 @@
+<?php
+function my_page_title() {
+    return 'WetStone Video Library'; // add dynamic content to this title (if needed)
+}
+add_action( 'pre_get_document_title', 'my_page_title' );
+?>
+
 <div class="page-preview">
 	<h2><a href="<?php the_permalink(); ?>" class="link link-body"><?php the_title(); ?></a></h2>
 
