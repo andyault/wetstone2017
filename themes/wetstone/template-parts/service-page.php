41f88bb7b6d4c9f81431fbd7f562627c6e7e0f3a
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-11-13 10:03:01 -0500

    Update service-page.php

--- themes/wetstone/template-parts/service-page.php
+++ themes/wetstone/template-parts/service-page.php
@@ -1,9 +1,3 @@
-<?php 
-function my_page_title() {
-    return 'Servicess'; // add dynamic content to this title (if needed)
-}
-add_action( 'pre_get_document_title', 'my_page_title' );
-?>
 <h2 class="section-header">
 	<a href="<?php the_permalink(); ?>" class="link link-body">
 		<?php the_title(); ?>
