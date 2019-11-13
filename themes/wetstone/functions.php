759f5b6e3cfd3e6a908b17a7ed5a2a09a963f9c7
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-11-13 09:46:37 -0500

    title

--- themes/wetstone/functions.php
+++ themes/wetstone/functions.php
@@ -1,9 +1,3 @@
 <?php
-function wpse62415_filter_wp_title( $title ) {
-    if ( is_page_template( 'video-library-page.php' ) ) {
-        return 'WetStone Video Library';
-    }
-    return $title;
-}
-add_filter( 'the_title', 'wpse62415_filter_wp_title' );
+
 ?>
