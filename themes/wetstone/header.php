6cc2ecdd1e7dd9b8183c7187c62b700fc88c6575
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2019-11-19 12:37:59 -0500

    Update header.php

--- themes/wetstone/header.php
+++ themes/wetstone/header.php
@@ -76,7 +76,7 @@
 
 		<header class="header" style="height:78px">
 			<div style="width: 100%; position:fixed;">
-				<div style="float: right; font-size: 14px; margin: 5px 5px 0px 0px; color: #AAAAAA"><p>WetStone Technologies, A Division of <a href="https://www.allencorporation.com/" class="link" style="color:green" title="Allen Corporation of America"> Allen Corporation</a></p>
+				<div style="float: right; font-size: 14px; margin: 5px 5px 0px 0px; color: #AAAAAA"><p>WetStone Technologies, A Division of <a href="https://www.allencorporation.com/" target="_blank" class="link" style="color:#1fb04c" title="Allen Corporation of America"> Allen Corporation</a></p>
 				</div>
 			</div>
 			<?php global $post; ?>
