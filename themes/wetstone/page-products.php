53110d061e162a0af8f2943c173056fbf826eff4
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2020-01-24 09:25:42 -0500

    Update page-products.php

--- themes/wetstone/page-products.php
+++ themes/wetstone/page-products.php
@@ -21,7 +21,7 @@
 
 			foreach($posts as $post) {
 				setup_postdata($post);
-				if($post->ID >= 612 && $post->ID <= 634 || $post->ID == 1054) { // IDs of unwanted products
+				if($post->ID >= 612 && $post->ID <= 634 || $post->ID == 1054 || $post->ID == 116) { // IDs of unwanted products
 					if($post->ID == 633) {
 						get_template_part('template-parts/' . $postType, 'preview');
 					}
@@ -38,7 +38,7 @@
 	<ul class="carousel-dots">
 		<?php
 			foreach($posts as $post)
-				if($post->ID >= 612 && $post->ID <= 634 || $post->ID == 1054) { // IDs of unwanted products
+				if($post->ID >= 612 && $post->ID <= 634 || $post->ID == 1054 || $post->ID == 116) { // IDs of unwanted products
 					if($post->ID == 633) {
 						echo '<li class="carousel-dot"></li>';
 					}
@@ -66,7 +66,7 @@
 				<?php
 					foreach($posts as $post) {
 						setup_postdata($post);
-						if($post->ID >= 612 && $post->ID <= 634 || $post->ID == 1054) { // IDs of unwanted products
+						if($post->ID >= 612 && $post->ID <= 634 || $post->ID == 1054 || $post->ID == 116) { // IDs of unwanted products
 							
 							if($post->ID == 633) {
 								get_template_part('template-parts/' . $postType, 'page');
