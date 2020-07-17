9207883eac51e580239a2ed285b710574eeb4104
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2020-07-17 08:51:06 -0400

    Update product-single.php

--- themes/wetstone/template-parts/product-single.php
+++ themes/wetstone/template-parts/product-single.php
@@ -5,7 +5,7 @@ $exTitle = explode(": ", get_the_title());
 <!-- todo: side buttons (overview, screenshots, free trial, etc) -->
 
 <section class="product-single-description">
-	<h1 class="section-header <?php if($post->ID != 141) echo 'wetstone-font'; ?>"><?php echo $exTitle[0] . "<br />" . $exTitle[1]; ?></h1><br />
+	<div style="min-height: 150px; height: auto"><h1 class="section-header <?php if($post->ID != 141) echo 'wetstone-font'; ?>"><?php echo $exTitle[0] . "<br />" . $exTitle[1]; ?></h1></div>
 
 	<div class="body-content">
 		<?php the_content(); ?>
