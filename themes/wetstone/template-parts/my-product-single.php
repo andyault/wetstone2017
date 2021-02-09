96130df4993653509c511871e4337b7d0ec16e80
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2021-02-09 09:53:13 -0500

    Update my-product-single.php

--- themes/wetstone/template-parts/my-product-single.php
+++ themes/wetstone/template-parts/my-product-single.php
@@ -16,11 +16,18 @@
 		<div style="background-image: url(<?php the_post_thumbnail_url('medium'); ?>);" class="myproduct-overview-image"></div>
 
 		<div class="myproduct-overview-info"><?php
-			if ($post->ID == 1815) {
+		if ($post->ID == 1815) {
 				the_excerpt(); 
-			} else {				
-				echo get_post_meta($post->ID, 'wetstone_product_customerexcerpt', true);
-			}
+		} 
+		
+		if ($post->ID == 633 || $post->ID == 634) {
+				echo '<a href="https://video.wetstonetech.com/video-categories/gargoyle-investigator/" target="_blank" border="0" class="link link-body"><img src="https://www.wetstonetech.com/wp-content/uploads/2021/01/gargVideoSite.png" alt="Gargoyle Investigator MP How To Videos" width="433" height="100"></a></center>';
+		} 
+		
+		if ($post->ID == 115 || $post->ID == 1807) {
+				echo '<a href="https://video.wetstonetech.com/video-categories/stegohuntmp/" target="_blank" border="0"><img src="https://www.wetstonetech.com/wp-content/uploads/2021/01/stegoVideoSite.png" alt="StegoHunt MP How To Videos" width="433" height="100"></a></center>';
+		} 
+		
 		?></div>
 	</div>
 
