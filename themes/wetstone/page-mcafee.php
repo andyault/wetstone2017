5f22b531ac3388473dede5ea593b4c5225d31bb3
wconklinACA <33701349+wconklinACA@users.noreply.github.com>
2021-02-11 13:17:12 -0500

    Create page-mcafee.php

---
+++ themes/wetstone/page-mcafee.php
@@ -0,0 +1,100 @@
+<?php
+	get_header();
+?>
+<section class="page-posts site-content site-content-padded">
+<h1 class="page-header">When Deploying McAfee - Experience Counts</h2>
+
+<p style="font-size: 16px;">For almost two decades <strong>WetStone Technologies, The Cyber Security Division of Allen Corporation</strong> has helped to protect our customers’ critical information assets by providing comprehensive consulting services for the best practices implementation and operation of McAfee security solutions.  </p>
+
+
+<div style="position: relative; overflow: hidden; padding-top: 56.25%;"><iframe id="vimeoVideoPlayer" style="position: absolute; top: 30px; left: 6%; width: 90%; height: 90%; border: 0;" src="https://player.vimeo.com/video/415132242" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>
+<div style="position: relative; width: 80%; left: 12%;">
+<p style="font-size: 14px;">Ken McCray, McAfee's Head of Channel Sales and Operations, discusses the WetStone team and the high standards McAfee demands from its Service Delivery Providers.</p>
+
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.boxW {
+	width:310px;
+	height:200px;
+	float: left;
+	padding: 10px;
+}
+
+.column {
+  width: 230px;
+  height: 180px; 
+  padding: 10px;
+  float: left;
+}
+.icons {
+  float: left;
+  width: 60px;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+</head>
+<body>
+
+
+
+<div class="row">
+	<div class="boxW">
+  <div class="icons"> <a href="https://www.wetstonetech.com/service/mcafee-solution-services-training/"class="link link-body" style="font-size: 16px;"><img src="https://www.wetstonetech.com/wp-content/uploads/2021/02/mcafee_training.png" border="0"/></a></div>
+  <div class="column" >
+       <a href="https://www.wetstonetech.com/service/mcafee-solution-services-training/"class="link link-body" style="font-size: 16px;">McAfee Training</a>
+<p style="font-size: 16px;">Instructional content and mentoring sessions delivered by McAfee certified subject matter experts </p>
+  </div>  
+  </div>
+  <div class="boxW">
+	<div class="icons"> <a href="https://www.wetstonetech.com/service/mcafee-professional-services/"class="link link-body" style="font-size: 16px;"><img src="https://www.wetstonetech.com/wp-content/uploads/2021/02/mcafee_serevices.png" border="0"/></a></div>
+  <div class="column">
+    <a href="https://www.wetstonetech.com/service/mcafee-professional-services/"class="link link-body" style="font-size: 16px;">McAfee Services</a>
+<p style="font-size: 16px;">McAfee professional services delivered by experienced subject matter experts across the entire spectrum of McAfee solutions.</p>
+  </div>
+    </div>
+  <div class="boxW">
+<div class="icons"> <a href="https://www.wetstonetech.com/products/mcafee-products/"class="link link-body" style="font-size: 16px;"><img src="https://www.wetstonetech.com/wp-content/uploads/2021/02/mcafee_solutions.png" border="0"/></a></div>
+  <div class="column" >
+   <a href="https://www.wetstonetech.com/products/mcafee-products/"class="link link-body" style="font-size: 16px;">McAfee Solutions</a>
+<p style="font-size: 16px;">McAfee products for protecting the critical IT assets of an enterprise.</p>
+  </div>
+    </div>
+  <div class="boxW">
+<div class="icons"> <a href="https://www.wetstonetech.com/service/mcafee-healthcheck-services/"class="link link-body" style="font-size: 16px;"><img src="https://www.wetstonetech.com/wp-content/uploads/2021/02/health_checks.png" border="0"/></a></div>
+  <div class="column" >
+    <a href="https://www.wetstonetech.com/service/mcafee-healthcheck-services/"class="link link-body" style="font-size: 16px;">McAfee Health Checks</a>
+<p style="font-size: 16px;">Services designed to review an existing McAfee environment for proper operation and conformance with best practices.</p>
+  </div>
+    </div>
+  <div class="boxW">
+<div class="icons"> <a href="https://www.wetstonetech.com/service/quick-start-solution-services/"class="link link-body" style="font-size: 16px;"><img src="https://www.wetstonetech.com/wp-content/uploads/2021/02/quick_starts.png" border="0"/></a></div>
+   <div class="column" >
+    <a href="https://www.wetstonetech.com/service/quick-start-solution-services/"class="link link-body" style="font-size: 16px;">McAfee Quick Starts</a>
+<p style="font-size: 16px;">Cost-effective remote services designed to get a McAfee product deployed efficiently and in accordance with best practices. </p>
+  </div>
+    </div>
+  <div class="boxW">
+  <div class="icons"> <a href="https://www.wetstonetech.com/service/mcafee-managed-services/"class="link link-body" style="font-size: 16px;"><img src="https://www.wetstonetech.com/wp-content/uploads/2021/02/managed_services.png" border="0"/></a></div>
+   <div class="column" >
+    <a href="https://www.wetstonetech.com/service/mcafee-managed-services/"class="link link-body" style="font-size: 16px;">McAfee Managed Services</a>
+<p style="font-size: 16px;">Deployment services and on-going management of McAfee Security products.</p>
+  </div>
+    </div>
+</div>
+
+</body>
+</html>
+</section>
+
+<?php
+	get_footer();
+?>
